3db8f99390991680ef14eb7ca0640309b6832c82
Eric Eisenhart <eric.eisenhart@sonic.com>
2024-05-06 17:20:01 -0700

    Initial framework coming together

--- src/sonic/netbox_zabbix/netbox_zabbix.py
+++ src/sonic/netbox_zabbix/netbox_zabbix.py
@@ -3,8 +3,13 @@ import pynetbox
 import configargparse
 import sys
 import pprint
+import logging
+import logging.handlers
 
-from sonic.logger import setup_sonic_logger
+try:
+   from sonic.logger import setup_sonic_logger
+except ModuleNotFoundError as err:
+   pass
 
 class NetboxZabbix():
     """
@@ -13,33 +18,54 @@ class NetboxZabbix():
     
     def __init__(self):
         """Set up argument parser and load sub-commands."""
-        epilog = """
-               General Options:
-                 -h, --help           Show this help message and exit
-                 -v, --verbose        Show more logging messages
-                 -q, --quiet          Show fewer logging messages"""
         try:
-            self.config = configargparse.ArgParser(
-                default_config_files=["/etc/sonic/netbox-zabbix.conf", "~/.sonic-netbox-zabbix.conf"],
+            argparser = configargparse.ArgParser(
+                default_config_files=["/etc/sonic/netbox-zabbix.conf", "~/.sonic-netbox-zabbix.conf", "./sonic-netbox-zabbix.conf"],
                 description="Sync netbox stuff to zabbix stuff.",
-                epilog=epilog,
             )
+            
+            argparser.add("-v", "--verbose", action="store_true", help="Show more logging messages")
+            argparser.add("-q", "--quiet", action="store_true", help="Show fewer logging messages")
+            argparser.add("--netboxurl", required=True, help="URL for netbox")
+            argparser.add("--zabbixurl", required=True, help="URL for zabbix")
+            argparser.add("--netboxtoken", required=True, help="API auth token for Netbox")
+            argparser.add("--zabbixtoken", required=True, help="API auth token for Zabbix")
+
+            self.config = argparser.parse_args()
         except Exception as e:
            print('An error occured parsing config files or command-line arguments: {}'.format(str(e)))
+           raise
 
         try:
-           self.logger = setup_sonic_logger(
+           self.log = setup_sonic_logger(
               application_name='Sonic Netbox Zabbix',
               logger_name='netbox_zabbix',
            )
-           self.logger.info("Starting Sonic Netbox Zabbix Sync")
         except Exception as e:
-           print('An error occured setting up logging: {}'.format(str(e)))
-           
+           try:
+               logging.basicConfig(
+                    level=logging.WARNING,
+                    format='%(name)s:%(levelno)s:%(funcName)s:%(message)s',
+               )
+               self.log = logging.getLogger('netbox_zabbix')
+               sysloghandler = logging.handlers.SysLogHandler(address = '/dev/log')
+               sysloghandler.setLevel(logging.INFO)
+               self.log.addHandler(sysloghandler)
+           except Exception as e2:
+               print('Unable to make logging go: {}'.format(str(e)))
+               raise
+           # pass
+
+        if self.config.verbose:
+           self.log.setLevel(logging.INFO)
+        elif self.config.quiet:
+           self.log.setLevel(logging.ERROR)
+
+        self.log.info("Starting Sonic Netbox Zabbix Sync")
 
     def run(self, argv):
         """Run cli app with the given arguments."""
-        self.config.print_help()
+        # self.config.print_help()
 
 def main():
    """Run NetboxZabbix cli with sys.argv from command line."""
